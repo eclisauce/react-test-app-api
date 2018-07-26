27e99f335cd0d90ef212c4315283fedd7a9b1575
Markus Andersson <make_99@hotmail.com>
2018-07-26 11:53:48 +0200

    Almost working db connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import auth from './routes/auth';
+
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost/react-test', {useMongoClient: true});
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({errors: {global: "Invalid credentials"}});
-}); 
+app.use('/api/auth', auth);
 
 app.get('*/', (req, res) =>{
   res.sendFile(path.join(__dirname, 'index.html'));
